0ede8c9be69cc12d128009744d9b524c16388ea6
DESKTOP-U2BJ55P\Sander <sanderxavalon@gmail.com>
2022-11-08 23:50:34 +0800

    Add default timestamp and update constraint

--- work-logger/src/main/resources/schema.sql
+++ work-logger/src/main/resources/schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE content (
     title VARCHAR(255) NOT NULL,
     content_type_fk INTEGER NOT NULL,
     content VARCHAR,
-    create_time TIMESTAMP(0) WITHOUT TIME ZONE,
-    update_time TIMESTAMP(0) WITHOUT TIME ZONE
+    create_time TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE content_type (
@@ -28,15 +28,15 @@ CREATE TABLE job (
     name VARCHAR(255) NOT NULL,
     job_status_fk INTEGER NOT NULL,
     link VARCHAR(255),
-    create_time TIMESTAMP(0) WITHOUT TIME ZONE
+    create_time TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE attached_file (
     id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     file_name VARCHAR(255) NOT NULL,
     file_url VARCHAR(255) NOT NULL
-)
+);
 
 CREATE TRIGGER CON_TYP AFTER INSERT ON content_type FOR EACH ROW CALL "com.sander.worklogger.trigger.H2Trigger";
 
-ALTER TABLE job ADD CONSTRAINT job_status_to_job_fk FOREIGN KEY (job_status_fk) REFERENCES job(id);
+ALTER TABLE job ADD CONSTRAINT job_status_to_job_fk FOREIGN KEY (job_status_fk) REFERENCES job_status(id);
