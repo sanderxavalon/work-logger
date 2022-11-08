01ac57fa2463ec9689588a1dd165cd70239d16ea
DESKTOP-U2BJ55P\Sander <sanderxavalon@gmail.com>
2022-11-08 23:22:16 +0800

    Add internal file reading example

--- work-logger/src/main/java/com/sander/worklogger/controller/ContentController.java
+++ work-logger/src/main/java/com/sander/worklogger/controller/ContentController.java
@@ -1,7 +1,13 @@
 package com.sander.worklogger.controller;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 public class ContentController {
@@ -9,5 +15,15 @@ public class ContentController {
     @GetMapping("/test")
     public String test(){
         return "Hello World";
+    }
+
+    @GetMapping(
+            value = "/get-image-with-media-type",
+            produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public byte[] getImageWithMediaType() throws IOException {
+        InputStream in = getClass()
+                .getResourceAsStream("/static/img/221002.jpg");
+        return IOUtils.toByteArray(in);
     }
 }
