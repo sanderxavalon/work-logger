e05aba21c77b49e13f665090779827fd3805da84
DESKTOP-U2BJ55P\Sander <sanderxavalon@gmail.com>
2022-11-17 00:05:08 +0800

    Add title and type field

--- vue-admin-template/src/views/markdown/index.vue
+++ vue-admin-template/src/views/markdown/index.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="app-container">
-    <v-md-editor
-      v-model="content"
-      :disabled-menus="[]"
-      height="600px"
-      @save="saveFromEditor"
-      @upload-image="handleUploadImage"
-    />
+    <el-row :gutter="20">
+      <el-col :span="20">
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+      </el-col>
+      <el-col :span="2">
+        <el-select v-model="value" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>      
+      </el-col>
+      <el-col :span="2">
+          <el-button type="primary"  style="float:right;">主要按钮</el-button>
+      </el-col>
+    </el-row>  
+    <el-row>
+      <el-col :span="24">
+      <v-md-editor
+        v-model="content"
+        :disabled-menus="[]"
+        height="600px"
+        width="100%"
+        @save="saveFromEditor"
+        @upload-image="handleUploadImage"
+      />
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -43,7 +66,24 @@ export default {
   },
   data: function() {
     return {
-      content: example
+      content: example,
+      options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+      input: ''
     }
   },
   methods: {
